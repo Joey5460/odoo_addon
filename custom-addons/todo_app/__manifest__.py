6d538a14111f459cb97b75158563ab3dac9cedc4
fosy <zhouyu5460@gmail.com>
2018-07-01 11:41:25 +0800

    init

---
+++ custom-addons/todo_app/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    'name':'ToDo Application',
+    'description':'Manage To-Do Tasks',
+    'author': 'Joey',
+    'depends': ['base'],
+    'application': True,
+    'data': ['views/todo_menu.xml'],
+}
---
+++ fedora.sh
@@ -0,0 +1,6 @@
+dnf.sh
+sudo postgresql-setup --initdb --unit postgresql
+sudo systemctl enable postgresql
+sudo systemctl start postgresql
+sudo su - postgres -c "createuser -s $USER"
+sudo npm install -g less
