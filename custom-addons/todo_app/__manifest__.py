fb5dfe45c7deccc55b322519d7ff96207c56bada
fosy <zhouyu5460@gmail.com>
2018-07-03 13:51:46 +0800

    deleted search in view

--- custom-addons/todo_app/__manifest__.py
+++ custom-addons/todo_app/__manifest__.py
@@ -7,7 +7,7 @@
     'data': [
         'security/ir.model.access.csv',
         'security/todo_access_rules.xml',
-        'views/todo_view.xml',
         'views/todo_menu.xml',
+        'views/todo_view.xml',
         ],
 }
