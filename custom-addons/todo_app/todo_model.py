c638fa3bf39953d7f1723347a906867b06de9fb0
fosy <zhouyu5460@gmail.com>
2018-07-05 11:12:40 +0800

    first table

--- custom-addons/todo_app/todo_model.py
+++ custom-addons/todo_app/todo_model.py
@@ -4,13 +4,13 @@ class TodoTask (models.Model):
     _name = 'todo.task'
     _description = 'To-do Task'
     name = fields.Char('Name', required=True) 
-    num = fields.Char('Number')
+    num = fields.Char('Number', required=True)
     scale = fields.Char('Scale')
     acc = fields.Char('Accurate')
     manu = fields.Char('Manufacture')
     lev_time = fields.Char('Leverage Time')
     det_peri= fields.Char('Detection Period')
-    exp = fields.Char('Expiration')
+    exp = fields.Char('Expiration',required=True)
     dep = fields.Char('Department')
     user = fields.Char('User')
     comment = fields.Text('comment')
@@ -18,19 +18,19 @@ class TodoTask (models.Model):
     det_no= fields.Integer('Detection No.')
     result= fields.Boolean('Result')
     issue_dep= fields.Boolean('Issue Dep')
-    is_done = fields.Boolean('Done?')
-    active = fields.Boolean('Active?', default=True)
+    #is_done = fields.Boolean('Done?')
+    #active = fields.Boolean('Active?', default=True)
 
     @api.multi
     def do_toggle_done(self):
-        for task in self:
-            task.is_done = not task.is_done
+        #for task in self:
+        #    task.is_done = not task.is_done
         return True
 
     #@api.model
     @api.multi
     def do_clear_done(self):
-        dones = self.search([('is_done', '=', True)])
-        dones.write({'active': False})
+        #dones = self.search([('is_done', '=', True)])
+        #dones.write({'active': False})
         return True
 
